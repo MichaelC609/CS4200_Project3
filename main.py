1784a8377ca8ea46e507404e0bd7ba08ea0674d4
Anna Yee <annah@nanner-nanner-nanner.local>
2026-04-28 20:29:29 -0700

    stuff

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import numpy as np
+
+# user input
+def getinput():
+    first = input("Would you like to go first? (y/n) : ")
+    while first != "y" and first != "n":
+        first = input("Would you like to go first? (y/n) : ")
+
+    # think = input("How long should the computer think about its moves (in seconds)? : ")
+    think = 5
+    return first, think
+
+# evaluation function
+
+# search tree 
+## alpha beta pruning
+
+# print board
+
+# main ()
